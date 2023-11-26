39dcba61f3e18d1ab7518c3d32656fbae58a90fa
rogue <rogue@PC>
2023-11-26 19:30:26 +0100

    Dodaj pliki projektów.

---
+++ Udemy_Medical_Bot_/MedicalBot.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Udemy_Medical_Bot_
+{
+    internal class MedicalBot
+    {
+        static string BotName = "Bob";
+
+
+        static string GetBotName()
+        {
+            return BotName;
+        }
+
+        //Powinien zwrócić leki jakie potrzebuje pacejnt
+        void PrescribeMedication(Patient patient)
+        {
+            //TODO
+            //If the patient's symptoms are a headache, prescribe ibuprofen.
+
+            //If the patient's symptoms are skin rashes, prescribe diphenhydramine.
+
+            //If the patient's symptoms are dizziness, check if the patient has a medical history of diabetes. If they do, prescribe metformin. Otherwise, prescribe dimenhydrinate.
+
+
+            GetDosage("", patient);
+        }
+
+        string GetDosage(string medicineName, Patient patient)
+        {
+            if (medicineName == "ibuprofen")
+            {
+                if(patient.)
+            }
+
+
+            return "";
+        }
+
+    }
+}
---
+++ Udemy_Medical_Bot_/Patient.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Udemy_Medical_Bot_
+{
+    class Patient
+    {
+        string Name { get; set; }
+        int Age { get; set; }
+        string Gender { get; set; }
+        string MedicalHistory { get; set; }
+        string SymptomCode { get; set; }
+        string Prescription { get; set; }
+
+
+        #region Name
+        string GetName()
+        {
+            return Name;
+        }
+
+        bool SetName(string name, out string errorMessage)
+        {
+            //Null statement
+            if(string.IsNullOrEmpty(name))
+            {
+                errorMessage = "Name cannot be null or empty!";
+                return false;
+            }
+
+            //Shorter than 2 letters
+            if(name.Length < 2)
+            {
+                errorMessage = "Name is to short!";
+                return false;
+            }
+
+            Name = name;
+            errorMessage = string.Empty;
+
+            return true;
+        }
+        #endregion Name
+
+        #region AGE
+        int GetAge()
+        {
+            return Age;
+        }
+
+        bool SetAge(int age, out string errorMessage)
+        {
+            //
+            if(age<0)
+            {
+                errorMessage = "Age cant be lower than 0";
+                return false;
+            }
+
+            if (age > 100)
+            {
+                errorMessage = "Age can't be more than 100";
+                return false;
+            }
+
+
+
+            Age = age;
+            errorMessage = string.Empty;
+
+            return true;
+        }
+
+        #endregion AGE
+
+        #region Gender
+
+        string GetGender()
+        {
+            return Gender;
+        }
+
+        bool SetGender(string gender, out string errorMessage)
+        {
+            if(gender.ToLower() != "male" || gender.ToLower() != "female" || gender.ToLower() != "other")
+            {
+                errorMessage = "Wrond gender written!";
+                return false;
+            }
+
+            Gender = gender;
+            errorMessage = string.Empty;
+            return true;
+        }
+        #endregion
+    }
+}
